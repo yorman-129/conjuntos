13f225dfba9d52bcaf0c93fba84687af01347513
yorman tobon <yorman_tobon82201@elpoli.edu.co>
2022-02-08 18:07:51 -0500

    commit solo falta operacion con universal

--- main.cpp
+++ main.cpp
@@ -21,12 +21,17 @@ struct Nodo
 int menuPpal();
 int menuMantenimiento();
 void crearConjunto(int, string);
+void crearConjuntoUniversal(int, string);
 void mostrarConjunto(string);
 bool validarParentesis(string);
 bool numerosRepetidos(int, string);
+bool numerosRepetidosUniversal(int, string);
 string postfijo(string);
 string resultado(string);
 string Union(string, string); // retorna nombre del nuevo conjunto unido
+string interseccion(string, string);
+string diferencia(string , string );
+string diferenciaSimetrica(string , string );
 
 int main()
 {
@@ -54,11 +59,17 @@ int main()
             nombre = expresion[i];
             cout << "Digita la cantidad de elementos en: " << nombre << endl;
             cin >> tamano;
+            nombre = expresion[i];
             crearConjunto(tamano, nombre);
         }
     }
 
     /// conjunto universal falta y validacion de elementos
+    cout << "Digita la cantidad de elementos en: U"  << endl;
+            cin >> tamano;
+    crearConjuntoUniversal(tamano,"u");
+    mostrarConjunto("u");
+
     // que cree todas las operaciones en todos los conjuntos y pa la mierda
     system("pause");
 
@@ -107,12 +118,11 @@ bool numerosRepetidos(int num, string nombre)
 {
     Nodo *aux = inicio;
 
-   
-        while (aux->nombre != nombre)
+    while (aux != NULL)
+    {
+
+        if (aux->nombre == nombre)
         {
-            aux = aux->siguienteConjunto;
-        }
-        if(aux->nombre==nombre){
             Nodo *aux2 = aux;
             while (aux2 != NULL)
             {
@@ -122,9 +132,32 @@ bool numerosRepetidos(int num, string nombre)
                 }
                 aux2 = aux2->siguiente;
             }
-        }else{
-            return false;
         }
+        aux = aux->siguienteConjunto;
+    }
+    return false;
+}
+
+bool numerosRepetidosUniversal(int num, string nombre)
+{
+
+    Nodo *aux = inicio;
+
+    while (aux != NULL)
+    {
+            Nodo *aux2 = aux;
+            while (aux2 != NULL)
+            {
+                if (num == aux2->dato)
+                {
+                    return true;
+                }
+                aux2 = aux2->siguiente;
+            }
+        aux = aux->siguienteConjunto;
+        }
+    
+    return false;
 }
 
 // funcion para la creacion de los conjuntos
@@ -208,6 +241,87 @@ void crearConjunto(int tamano, string nombre)
         }
     }
 }
+
+void crearConjuntoUniversal(int tamano, string nombre){
+   Nodo *conjunto = new Nodo();
+    conjunto->nombre = nombre;
+    if (inicio == NULL)
+    {
+        for (int i = 0; i < tamano; i++)
+        {
+            int dato = 0;
+            Nodo *nuevo = new Nodo(); // creamos el nuevo Nodo
+            Nodo *aux = inicio;
+            Nodo *aux2 = NULL;
+
+            do
+            {
+                cout << "Digita el numero a insertar en el conjunto: ";
+                cin >> dato;
+            } while (numerosRepetidosUniversal(dato, nombre));
+
+            nuevo->dato = dato;
+            cout<<".";
+
+            while ((aux != NULL) and (aux->dato < dato))
+            {
+                aux2 = aux;
+                aux = aux->siguiente;
+            }
+
+            if (inicio == NULL)
+            {
+                inicio = conjunto;
+                cola = inicio;
+                inicio->siguiente = nuevo;
+            }
+            else
+            {
+                aux2->siguiente = nuevo;
+                nuevo->siguiente = aux;
+            }
+        }
+    }
+    else
+    {
+
+        cola->siguienteConjunto = conjunto;
+        cola = conjunto;
+
+        for (int i = 0; i < tamano; i++)
+        {
+            int dato = 0;
+            Nodo *nuevo = new Nodo(); // creamos el nuevo Nodo
+            Nodo *aux = cola;
+            Nodo *aux2 = NULL;
+
+            do
+            {
+                cout << "Digita el numero a insertar en el conjunto: ";
+                cin >> dato;
+            } while (numerosRepetidosUniversal(dato, nombre));
+
+            nuevo->dato = dato;
+
+            while ((aux != NULL) and (aux->dato < dato))
+            {
+                aux2 = aux;
+                aux = aux->siguiente;
+            }
+
+            if (aux == cola)
+            {
+                aux2 = conjunto;
+                aux2->siguiente = nuevo;
+            }
+            else
+            {
+                aux2->siguiente = nuevo;
+                nuevo->siguiente = aux;
+            }
+        }
+    }
+    }
 
 /////funcion para mostrar los conjuntos conjuntos
 void mostrarConjunto(string conjunto)
@@ -354,15 +468,19 @@ string resultado(string expresion)
                 solucion[top] = ' ';
                 top--;
             }
-
+string Op1="";
+string Op2="";
             switch (expresion[i])
             {
             case '^':
                 top++;
-
+             Op1 = diferencia(op1,op2);
+             Op2 = diferencia(op2,op1);
+             solucion[top] = Union(Op1, Op2);
                 break;
             case '*':
                 top++;
+                solucion[top] = interseccion(op1, op2);
                 break;
             case '/':
                 top++;
@@ -374,6 +492,7 @@ string resultado(string expresion)
                 break;
             case '-':
                 top++;
+                solucion[top] = diferencia(op1, op2);
                 break;
             default:
                 break;
@@ -474,3 +593,160 @@ string Union(string op1, string op2)
     cola = nuevo;
     return nuevo->nombre;
 }
+
+string interseccion(string op1, string op2)
+{
+    cout << "entro interseccion\n";
+    Nodo *aux = inicio;
+    Nodo *aux2 = NULL;
+    Nodo *aux3 = NULL;
+    string nom = "";
+
+    nom.append(op1);
+    nom.append("*");
+    nom.append(op2);
+
+    Nodo *nuevo = new Nodo();
+    nuevo->nombre = nom;
+    cola->siguienteConjunto = nuevo;
+    cola = nuevo;
+    cout << endl
+         << nuevo->nombre << endl;
+    system("pause");
+    while (aux != NULL)
+    {
+        if (aux->nombre == op1)
+        {
+            aux2 = aux;
+
+            if (nuevo->siguiente == NULL)
+            {
+                aux2 = aux2->siguiente;
+                Nodo *auxx = nuevo;
+                while (aux2 != NULL)
+                {
+
+                    if (numerosRepetidos(aux2->dato, op2))
+                    {
+                        Nodo *n = new Nodo();
+                        n->dato = aux2->dato;
+                        auxx->siguiente = n;
+                        auxx = auxx->siguiente;
+                        aux2 = aux2->siguiente;
+                    }
+                    else
+                    {
+                        aux2 = aux2->siguiente;
+                    }
+                }
+            }
+            else
+            {
+                aux3 = nuevo;
+                while (aux3->siguiente != NULL)
+                {
+                    aux3 = aux3->siguiente;
+                }
+                aux2 = aux2->siguiente;
+                while (aux2 != NULL)
+                {
+                    Nodo *n = new Nodo();
+                    if (numerosRepetidos(aux2->dato, nom))
+                    {
+                        n->dato = aux2->dato;
+                        aux3->siguiente = n;
+                        aux3 = aux3->siguiente;
+                        aux2 = aux2->siguiente;
+                    }
+                    else
+                    {
+                        aux2 = aux2->siguiente;
+                    }
+                }
+            }
+        }
+
+        aux = aux->siguienteConjunto;
+    }
+
+    return nuevo->nombre;
+}
+
+string diferencia(string op1, string op2)
+{
+    cout << "entro interseccion\n";
+    Nodo *aux = inicio;
+    Nodo *aux2 = NULL;
+    Nodo *aux3 = NULL;
+    string nom = "";
+
+    nom.append(op1);
+    nom.append("-");
+    nom.append(op2);
+
+    Nodo *nuevo = new Nodo();
+    nuevo->nombre = nom;
+    cola->siguienteConjunto = nuevo;
+    cola = nuevo;
+    cout << endl
+         << nuevo->nombre << endl;
+    system("pause");
+    while (aux != NULL)
+    {
+        if (aux->nombre == op1)
+        {
+            aux2 = aux;
+
+            if (nuevo->siguiente == NULL)
+            {
+                aux2 = aux2->siguiente;
+                Nodo *auxx = nuevo;
+                while (aux2 != NULL)
+                {
+
+                    if (numerosRepetidos(aux2->dato, op2))
+                    {
+                        aux2 = aux2->siguiente;
+                    }
+                    else
+                    {
+                        Nodo *n = new Nodo();
+                        n->dato = aux2->dato;
+                        auxx->siguiente = n;
+                        auxx = auxx->siguiente;
+                        aux2 = aux2->siguiente;
+                    }
+                }
+            }
+            else
+            {
+                aux3 = nuevo;
+                while (aux3->siguiente != NULL)
+                {
+                    aux3 = aux3->siguiente;
+                }
+                aux2 = aux2->siguiente;
+                while (aux2 != NULL)
+                {
+                    Nodo *n = new Nodo();
+                    if (numerosRepetidos(aux2->dato, nom))
+                    {
+                        n->dato = aux2->dato;
+                        aux3->siguiente = n;
+                        aux3 = aux3->siguiente;
+                        aux2 = aux2->siguiente;
+                    }
+                    else
+                    {
+                        aux2 = aux2->siguiente;
+                    }
+                }
+            }
+        }
+
+        aux = aux->siguienteConjunto;
+    }
+
+    return nuevo->nombre;
+}
+
